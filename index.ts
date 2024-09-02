ae985ea06c26235f9165890c8f6d158263050f66
utkarsh.pancholi <utkarsh.pancholi@celebaltech.com>
2024-09-02 22:58:51 +0530

    fix: updated global worker

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { pdfjs } from "react-pdf";
 
 // Path to the pdf.worker.js file
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.mjs`;
 
 /**
  * Extracts text content from a PDF file.
