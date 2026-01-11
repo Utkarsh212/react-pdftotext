87e151d60e791182ad3858d224e03462b546eeee
Utkarsh212 <utkarsh21200@gmail.com>
2026-01-11 22:36:28 +0530

    chore: updated gh-pages config

--- homepage/vite.config.ts
+++ homepage/vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/react-pdftotext/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
